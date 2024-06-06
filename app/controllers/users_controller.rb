729e52b4e7e311b9562f32f38fe64f8ff2eb5f8f
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-06 07:37:07 +0000

    devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,49 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :search, :search_results]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :liked_posts]
+
   def mypage
+    @user = current_user
+    @posts = @user.posts
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to mypage_users_path, notice: 'プロフィールを更新しました。'
+    else
+      render :edit
+    end
+  end
+
   def show
   end
 
-  def update
-  end
-
   def destroy
+    @user.destroy
+    redirect_to root_path, notice: 'ユーザーアカウントを削除しました。'
   end
 
   def liked_posts
+    @liked_posts = @user.liked_posts
   end
 
   def search
   end
 
   def search_results
+    @users = User.where('name LIKE ?', "%#{params[:q]}%")
   end
 
-  def guest_sign_in
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
