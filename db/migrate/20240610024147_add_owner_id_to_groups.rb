3c2a973277ef242adf5950c6fd527c30b8a3c682
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-18 10:34:06 +0000

    [Fix] migration

--- db/migrate/20240610024147_add_owner_id_to_groups.rb
+++ db/migrate/20240610024147_add_owner_id_to_groups.rb
@@ -1,6 +1,6 @@
 class AddOwnerIdToGroups < ActiveRecord::Migration[6.1]
   def change
-    add_column :groups, :owner_id, :integer
-    add_foreign_key :groups, :users, column: :owner_id
+    # add_column :groups, :owner_id, :integer
+    # add_foreign_key :groups, :users, column: :owner_id
   end
 end
