cbb6eb57d73aeaaceffc5210a32ae5bde5a9c97e
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-18 15:43:28 +0000

    [fix]controller

---
+++ app/controllers/users/user_groups_controller.rb
@@ -0,0 +1,23 @@
+class UserGroupsController < ApplicationController
+  def create
+    @group = Group.find(params[:group_id])
+    @user_group = @group.user_groups.build(user: current_user)
+    if @user_group.save
+      redirect_to group_path(@group), notice: 'グループに参加しました。'
+    else
+      redirect_to group_path(@group), alert: 'グループに参加できませんでした。'
+    end
+  end
+
+  def destroy
+    @user_group = UserGroup.find(params[:id])
+    @user_group.destroy
+    redirect_to group_path(@user_group.group), notice: 'グループから退出しました。'
+  end
+
+  private
+
+  def user_group_params
+    params.require(:user_group).permit(:user_id, :group_id)
+  end
+end
