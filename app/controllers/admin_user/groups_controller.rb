0d2a8269295a6dd3414d7fe772a8e7d4828fc3ba
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-24 05:57:46 +0000

    [Fix]group

--- app/controllers/admin_user/groups_controller.rb
+++ app/controllers/admin_user/groups_controller.rb
@@ -1,37 +1,50 @@
-class AdminU::GroupsController < ApplicationController
-    layout 'admin'
+# app/controllers/admin_user/groups_controller.rb
+class AdminUser::GroupsController < ApplicationController
+  layout 'admin'
 
-    def index
-      @groups = Group.all
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to admin_user_group_path(@group), notice: 'Group was successfully created.'
+    else
+      render :new
     end
+  end
 
-    def show
-      @group = Group.find(params[:id])
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to admin_user_group_path(@group), notice: 'Group was successfully updated.'
+    else
+      render :edit
     end
+  end
 
-    def edit
-      @group = Group.find(params[:id])
-    end
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to admin_user_groups_path, notice: 'Group was successfully destroyed.'
+  end
 
-    def update
-      @group = Group.find(params[:id])
-      if @group.update(group_params)
-        redirect_to admin_group_path(@group)
-      else
-        render :edit
-      end
-    end
+  private
 
-    def destroy
-      @group = Group.find(params[:id])
-      @group.destroy
-      redirect_to admin_groups_path
-    end
-
-    private
-
-    def group_params
-      params.require(:group).permit(:name, :description)
-    end
+  def group_params
+    params.require(:group).permit(:name, :description)
   end
 end
