776487a0c6fff55970c0a75f7eb23ab74121ac75
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-24 07:17:41 +0000

    [Fix]owner

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   # Associations
-  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id', optional: false
+  belongs_to :owner, class_name: 'User', foreign_key: 'new_owner_id', optional: false
   has_one_attached :image
   has_many :user_groups
   has_many :users, through: :user_groups
