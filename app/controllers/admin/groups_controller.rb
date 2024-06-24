cf69a90236e0d38c0e0b257c7a1c06ed709f3a57
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-24 05:04:32 +0000

    [Fix]group

---
+++ app/controllers/admin/groups_controller.rb
@@ -0,0 +1,38 @@
+module Admin
+  class GroupsController < ApplicationController
+    layout 'admin'
+
+    def index
+      @groups = Group.all
+    end
+
+    def show
+      @group = Group.find(params[:id])
+    end
+
+    def edit
+      @group = Group.find(params[:id])
+    end
+
+    def update
+      @group = Group.find(params[:id])
+      if @group.update(group_params)
+        redirect_to admin_group_path(@group)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @group = Group.find(params[:id])
+      @group.destroy
+      redirect_to admin_groups_path
+    end
+
+    private
+
+    def group_params
+      params.require(:group).permit(:name, :description, :image_url)
+    end
+  end
+end
