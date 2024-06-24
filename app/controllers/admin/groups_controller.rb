181d656f47f1e033bc69ee025b8f986e84d98e92
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-24 05:32:45 +0000

    [Fix]admin

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,38 +1,38 @@
 module Admin
-  class UsersController < ApplicationController
+  class GroupsController < ApplicationController
     layout 'admin'
 
     def index
-      @users = User.all
+      @groups = Group.all
     end
 
     def show
-      @user = User.find(params[:id])
+      @group = Group.find(params[:id])
     end
 
     def edit
-      @user = User.find(params[:id])
+      @group = Group.find(params[:id])
     end
 
     def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        redirect_to admin_user_path(@user)
+      @group = Group.find(params[:id])
+      if @group.update(group_params)
+        redirect_to admin_group_path(@group)
       else
         render :edit
       end
     end
 
     def destroy
-      @user = User.find(params[:id])
-      @user.destroy
-      redirect_to admin_users_path
+      @group = Group.find(params[:id])
+      @group.destroy
+      redirect_to admin_groups_path
     end
 
     private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    def group_params
+      params.require(:group).permit(:name, :description)
     end
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,37 +1,38 @@
-# app/controllers/admin/users_controller.rb
-class Admin::UsersController < ApplicationController
-  layout 'admin'
+module Admin
+  class UsersController < ApplicationController
+    layout 'admin'
 
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to admin_user_path(@user)
-    else
-      render :edit
+    def index
+      @users = User.all
     end
-  end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to admin_users_path
-  end
+    def show
+      @user = User.find(params[:id])
+    end
 
-  private
+    def edit
+      @user = User.find(params[:id])
+    end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to admin_user_path(@user)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to admin_users_path
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
   end
 end
