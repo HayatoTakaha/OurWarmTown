8e3daff1cbd0772fea66d1a32672be4aec3093f0
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-24 06:11:10 +0000

    [Fix]user

--- app/controllers/admin_user/users_controller.rb
+++ app/controllers/admin_user/users_controller.rb
@@ -1,37 +1,37 @@
-class AdminUser::GroupsController < ApplicationController 
-    layout 'admin'
+# app/controllers/admin_user/users_controller.rb
+class AdminUser::UsersController < ApplicationController
+  layout 'admin'
 
-    def index
-      @users = User.all
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: 'User was successfully updated.'
+    else
+      render :edit
     end
+  end
 
-    def show
-      @user = User.find(params[:id])
-    end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_users_path, notice: 'User was successfully destroyed.'
+  end
 
-    def edit
-      @user = User.find(params[:id])
-    end
+  private
 
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        redirect_to admin_user_path(@user)
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @user = User.find(params[:id])
-      @user.destroy
-      redirect_to admin_users_path
-    end
-
-    private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
