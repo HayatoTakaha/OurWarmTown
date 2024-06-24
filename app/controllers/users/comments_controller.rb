6dd2847169dad6b516e736ab79bc48a0aa821b49
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-06-24 09:59:48 +0000

    [Fix]comments

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -1,24 +1,27 @@
-class CommentsController < ApplicationController
-  def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
-    @comment.user = current_user
+module Users
+  class CommentsController < ApplicationController
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.build(comment_params)
+      @comment.user = current_user
 
-    if @comment.save
-      respond_to do |format|
-        format.html { redirect_to @post }
-        format.js
-      end
-    else
-      respond_to do |format|
-        format.html { render 'posts/show' }
+      if @comment.save
+        respond_to do |format|
+          format.html { redirect_to @post }
+          format.js
+        end
+      else
+        respond_to do |format|
+          format.html { render 'posts/show' }
+          format.js { render 'comments/fail_create.js.erb' }
+        end
       end
     end
-  end
 
-  private
+    private
 
-  def comment_params
-    params.require(:comment).permit(:content)
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
   end
 end
